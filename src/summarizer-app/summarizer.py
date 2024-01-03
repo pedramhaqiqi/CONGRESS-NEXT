b5e93cb9dce431a467d22e6ae5fc33dcb93314ca
Pedram Haqiqi <63091219+pedramhaqiqi@users.noreply.github.com>
2024-01-03 12:49:32 -0500

    Refactor

--- src/summarizer-app/summarizer.py
+++ src/summarizer-app/summarizer.py
@@ -97,29 +97,31 @@ def fetch_image_from_title(title):
     print(response.json())
     return response.json()['data'][0]['url']
 
+
+def process_files(directory, memory):
+    pattern = re.compile(r"/([^/]+)$")
+    for file_path in yield_files(directory):
+        print('files yielded', file_path)
+        match = pattern.search(file_path)
+        if not match:
+            raise FileNotFoundError(f"No match found for {file_path}")
+        last_part = match.group(1)
+        if last_part not in memory['memory']:
+            process_file(file_path, last_part, memory)
+
+def process_file(file_path, last_part, memory):
+    with open(file_path, 'r', encoding='utf-8') as article_file:
+        result = _fetch_title_and_summaries(article_file.read())
+        image_url = fetch_image_from_title(result['four_sentence_summary'])
+        result['image_url'] = image_url
+    # Script.js reads from stdout
+    print(json.dumps(result))
+    memory['memory'].append(last_part)
+    with open('memory.json', 'w') as memory_file:
+        json.dump(memory, memory_file)
+
 if __name__ == "__main__":
     # Checks if the files in articles have been processed before, and only processes the new ones
-    pattern = r"/([^/]+)$"
-    with open('memory.json', 'r') as file:
-        memory = json.load(file)
-
-    for file in yield_files('articles'):
-        print('files yielded', file)
-        match = re.search(pattern, file)
-        if not match:
-            raise Exception("No match found Error")
-        last_part = match.group(1)
-        print('last', last_part)
-        if last_part not in [file for file in memory['memory']]:
-            print('not in memory')
-            with open(file, 'r', encoding='utf-8') as file:        
-                result = _fetch_title_and_summaries(file.read())
-                print('result', result)
-                image_url = fetch_image_from_title(result['four_sentence_summary'])
-                result['image_url'] = image_url
-            print(json.dumps(result))
-            memory['memory'].append(last_part)
-            with open('memory.json', 'w') as file:
-                json.dump(memory, file)
-            exit(0)
-    exit(1)
+    with open('memory.json', 'r') as memory_file:
+        memory = json.load(memory_file)
+        process_files('articles', memory)
